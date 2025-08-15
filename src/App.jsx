614fa9c76f1cd4651e62d57acab545c13818d088
vinudasenith <ha.vinudas@gmail.com>
2025-08-15 17:52:42 +0530

    integrate all sections into App component layout✌️

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Technologies from "./components/Technologies.jsx";
+import Projects from "./components/Projects.jsx";
+import Education from "./components/Education.jsx";
+import Contact from "./components/Contact.jsx";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Technologies />
+      <Projects />
+      <Education />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
